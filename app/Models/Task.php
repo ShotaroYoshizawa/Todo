7f3cece316cc423e8b52cac9643ef424af442703
shotaro <2b.sytr202@gmail.com>
2024-03-22 21:54:06 +0900

    その他のフォルダを追加

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -72,4 +72,5 @@ class Task extends Model
         return Carbon::createFromFormat('Y-m-d', $this->attributes['due_date'])
             ->format('Y/m/d');
     }
+
 }
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -8,24 +8,49 @@
           <div class="panel-heading">フォルダ</div>
           <div class="panel-body">
             <a href="{{ route('folders.create') }}" class="btn btn-default btn-block">
-              フォルダを追加する
+              フォルダを追加
             </a>
           </div>
           <div class="list-group">
+            
             <table class="table foler-table">
             <thead>
               <tr>
-                <th>フォルダ名</th>
-                <th>編集</th>
-                <th>削除</th>
+                <th>現在のフォルダ</th>
+                <th></th>
+                <th></th>
               </tr>
             </thead>
             <tbody>
               @foreach($folders as $folder)
-              <tr>
+              <tr class = "folder-lists {{ $current_folder_id !== $folder->id ? 'no-active' : '' }}">
                 <td>
                   <a href="{{ route('tasks.index', ['id' => $folder->id]) }}"
                     class="list-group-item {{ $current_folder_id === $folder->id ? 'active' : '' }}"
+                    >
+                    {{ $folder->title }}
+                  </a>
+                </td>
+                <td><a href="{{ route('folders.edit', ['id' => $folder->id]) }}">編集</a></td>
+                <td><a href="{{ route('folders.delete', ['id' => $folder->id]) }}">削除</a></td>
+              </tr>
+              @endforeach
+            </tbody>
+            </table>
+            <table class="table foler-table">
+            <thead>
+              <tr>
+                <th>その他のフォルダ</th>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach($folders as $folder)
+              <tr class = "folder-lists {{ $current_folder_id === $folder->id ? 'active' : '' }}">
+                <td>
+                  <a href="{{ route('tasks.index', ['id' => $folder->id]) }}"
+                    class="list-group-item"
                   >
                     {{ $folder->title }}
                   </a>
@@ -39,13 +64,14 @@
           </div>
         </nav>
       </div>
+
       <div class="column col-md-8">
         <div class="panel panel-default">
           <div class="panel-heading">タスク</div>
           <div class="panel-body">
             <div class="text-right">
               <a href="{{ route('tasks.create', ['id' => $current_folder_id]) }}" class="btn btn-default btn-block">
-                タスクを追加する
+                タスクを追加
               </a>
             </div>
           </div>
@@ -55,8 +81,9 @@
               <th>タイトル</th>
               <th>状態</th>
               <th>期限</th>
-              <th>編集</th>
-              <th>削除</th>
+              <th></th>
+              <th></th>
+              <th>作成日時</th>
             </tr>
             </thead>
             <tbody>
@@ -69,6 +96,7 @@
                 <td>{{ $task->formatted_due_date }}</td>
                 <td><a href="{{ route('tasks.edit', ['id' => $current_folder_id, 'task_id' => $task->id]) }}">編集</a></td>
                 <td><a href="{{ route('tasks.delete', ['id' => $task->folder_id, 'task_id' => $task->id]) }}">削除</a></td>
+                <td>{{ $task->created_at }}</td>
               </tr>
             @endforeach
             </tbody>
